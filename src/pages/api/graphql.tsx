84a3bc9eb29c9abe1c29b1e392d98b1942adf6e0
Dobrosvet <dimaprokofev@yandex.ru>
2024-11-15 21:53:31 +0300

    пример отдачи данных и цепочек вложенных типов (в т.ч. бесконечно)

--- src/pages/api/graphql.tsx
+++ src/pages/api/graphql.tsx
@@ -12,12 +12,24 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const db = [
+  { id: "1", content: "Блок 1", to: ["2"]},
+  { id: "4", content: "Блок 4" },
+  { id: "2", content: "Блок 2", from: ["1", "4"], to: ["3"] },
+  { id: "3", content: "Блок 3", from: ["2"], to: ["1"] },
+]
 
 const resolvers = {
   Query: {
     hello: () => 'world',
+    blocks: () => db,
+    block: (_: any, args: any) => db.find(block => args.id == block.id),
   },
-};
+  Block: {
+    from: (parent: any) => db.filter(block => parent.from?.includes(block.id)),
+    to: (parent: any) => db.filter(block => parent.to?.includes(block.id)),
+  }
+}
 
 const schema = loadSchemaSync(join(__dirname, './schema.graphql'), {
   loaders: [new GraphQLFileLoader()]
